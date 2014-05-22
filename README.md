7baebb4eb95428a703b4c9dcdf3d6d8bf22ae44e
jobukkit <jop@inbox.com>
2014-05-22 19:41:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+This is a Minecraft (Bukkit) plugin that allows for push notifications from your Minecraft via Pushover.
+
+Installation
+-----
+1. Put the plugin in your plugins folder.
+2. Reload or restart your server.
+
+Usage
+----
+Join and type the command: `/pushover enable <pushover user key>`. Get your Pushover user key by logging in at [http://www.pushover.net/](http://www.pushover.net/), copying it, and then pasting it in the chat using CTRL+V _(even if you have a Mac :/)_. Your players can (and should :) ) do this too! Enjoy!
+
+You can disable notifications again at any time using `/pushover disable`.
+
+Features
+-----
+
+- Get a notification when someone joins or leaves.
+   - Custom join/leave messages supported!
+
+- Broadcast to all online players and all offline players with notifications enabled with `/broadcast <message>`. Permission is `minecraftpush.broadcast`; by default all ops can do it.
+- More features coming!
+- [Name changing](https://mojang.com/2014/04/minecraft-1-7-6-pre-release/) ready!
+
+---------
+
+###Notes for users of very old Minecraft versions (for example Tekkit Classic)
+####On a fairly recent Minecraft version? Don't read this. It doesn't contain anything even remotely useful for you. Otherwise:
+
+- The plugin will crash by default on very old versions, due to Plugin Metrics being incompatible. Change `opt-out` to `true` in config.yml in the Plugin Metrics folder in the plugins folder to fix this. Only do this if it crashes, however.
+- When resetting your world, everyone will need to re-enter their Pushover user keys. This is because of the way old Bukkit versions handle player UUID's.
+
+-----
+This plugin is powered by [pushover4j](https://github.com/sps/pushover4j).
---
+++ src/net/jopv/minecraft/bukkit/minecraftpush/MinecraftPush.java
@@ -0,0 +1,270 @@
+package net.jopv.minecraft.bukkit.minecraftpush;
+
+import net.pushover.client.PushoverException;
+import net.pushover.client.PushoverMessage;
+import net.pushover.client.PushoverRestClient;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.mcstats.MetricsLite;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+
+/**
+ * @author Jop Vernooij
+ */
+public class MinecraftPush extends JavaPlugin implements Listener
+{
+	PushoverRestClient pushoverRestClient = new PushoverRestClient();
+
+	private FileConfiguration userKeysFileConfiguration = null;
+	private File userKeysFile = null;
+
+	public int invalidUsers;
+
+	public String titleEnd = null;
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
+		int users = getUserKeysFileConfiguration().getKeys(false).size();
+		if (users != 1)
+			getLogger().info(Integer.toString(users) + " users registered.");
+		else
+			getLogger().info("1 user registered.");
+
+		int invalidUsers = getAmountOfInvalidUsers();
+		if (invalidUsers > 0)
+		{
+			if (invalidUsers != 1)
+				getLogger().warning(Integer.toString(invalidUsers) + " users have apparently entered invalid Pushover user keys! They will not receive any notifications. Their user keys will be deleted as soon as they connect with the server again, so they can be warned.");
+			else
+				getLogger().warning("1 user has apparently entered an invalid Pushover user key! It will not receive any notifications. It's user key will be deleted as soon as it connects with the server again, so it can be warned.");
+		}
+
+		if (getServer().getServerName().equals("Unknown Server"))
+			getLogger().info("Your Minecraft is unnamed. If your Minecraft has a name, you should set it in server.properties, and then reload or restart your server.");
+		else
+			titleEnd = " (" + getServer().getServerName() + ")";
+
+		// Plugin Metrics
+		try {
+			MetricsLite m = new MetricsLite(this);
+			m.start();
+		}
+		catch (Exception e) {
+			// Cannot upload data :(
+		}
+	}
+
+	int getAmountOfInvalidUsers()
+	{
+		int counter = 0;
+
+		for (Map.Entry entry : getUserKeysFileConfiguration().getValues(false).entrySet()) {
+			if (entry.getValue().equals("INVALID")) {
+				counter++;
+			}
+		}
+
+		invalidUsers = counter;
+		return counter;
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("pushover"))
+		{
+			if (! (sender instanceof Player))
+			{
+				sender.sendMessage("You can only enable or disable notifications as a player.");
+				return true;
+			}
+
+			if (getCmdArgsLength(args) == 1)
+			{
+				if (args[0].equalsIgnoreCase("enable"))
+				{
+					if (sender.hasPermission("minecraftpush.receive"))
+					{
+						// Unfortunately, Pushover user key validation is not yet possible: https://github.com/sps/pushover4j/issues/3
+						getUserKeysFileConfiguration().set(((Player) sender).getUniqueId().toString(), args[1]);
+						saveUserKeysFile();
+
+						sender.sendMessage("§aPush notifications enabled! §fYou can disable them at any time using /pushover disable.");
+						return true;
+					}
+					else
+					{
+						sender.sendMessage("§cYou don't have permission to receive push notifications!");
+						return true;
+					}
+				}
+			}
+			else if (getCmdArgsLength(args) == 0)
+				if (args[0].equalsIgnoreCase("disable"))
+				{
+					getUserKeysFileConfiguration().set(((Player) sender).getUniqueId().toString(), null);
+					saveUserKeysFile();
+
+					sender.sendMessage("§aPush notifications disabled.");
+					return true;
+				}
+
+			return false;
+		}
+
+		if (cmd.getName().equalsIgnoreCase("minecraftpush"))
+		{
+			sender.sendMessage(new String[]{
+					"Use the /pushover command to enable or disable push notifications.",
+					"MinecraftPush, Copyright (C) Jop Vernooij, 2014. http://www.jopv.net/ " +
+					"This is NOT an official Pushover app. Pushover is a trademark and product of Superblock, LLC. " +
+					"This plugin is powered by pushover4j: https://github.com/sps/pushover4j " +
+					"Source available at https://github.com/Jop-V/MinecraftPush/. " +
+					"If this plugin is an important part of your Minecraft, please donate a little bitcoin: :) 1LkJKBJuadQxdZN46yuyWzn2kncSLm1tvU"});
+			return true;
+		}
+
+		if (cmd.getName().equalsIgnoreCase("broadcast"))
+		{
+			if (sender.hasPermission("minecraftpush.broadcast"))
+			{
+				StringBuilder builder = new StringBuilder();
+
+				for (String s : args)
+					builder.append(s).append(' ');
+
+				String message = builder.toString().trim();
+
+				getServer().broadcastMessage("§d[Broadcasting] " + message);
+				push(message);
+			}
+			else sender.sendMessage("§cYou don't have permission to broadcast!");
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Workaround for some really weird behavior.
+	 */
+	public int getCmdArgsLength(String[]args)
+	{
+		if (args != null)
+			return args.length - 1;
+		else
+			return -1;
+	}
+	
+	@EventHandler (priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent e)
+	{
+		push( e.getJoinMessage() );
+
+		// Invalid Pushover user key warning.
+		if (invalidUsers == 0)
+			return;
+		Map map = getUserKeysFileConfiguration().getValues(false);
+		UUID uuid = e.getPlayer().getUniqueId();
+		if (map.containsKey(uuid.toString()) && map.get(uuid.toString()).equals("INVALID"))
+		{
+			getOnlinePlayerByUuid(uuid).sendMessage("§cYour Pushover user key turned out te be invalid! Sorry for not telling you earlier. Please re-enable notifications with a valid Pushover user key.");
+			getUserKeysFileConfiguration().set(uuid.toString(), null);
+			saveUserKeysFile();
+		}
+	}
+
+	@EventHandler (priority = EventPriority.MONITOR)
+	public void onPlayerQuit(PlayerQuitEvent e)
+	{
+		push( e.getQuitMessage() );
+	}
+
+	/**
+	 * Push a message to all offline players with push notifications enabled.
+	 * @param message The message to be sent. Color codes will be automatically stripped out.
+	 */
+	@SuppressWarnings("SuspiciousMethodCalls")
+	public void push(String message)
+	{
+		Map<String, Object> map = getUserKeysFileConfiguration().getValues(false);
+
+		for (String playerUuidString : map.keySet())
+		{
+			UUID playerUuid = UUID.fromString(playerUuidString);
+			Player player = getOnlinePlayerByUuid(playerUuid);
+			String playerPushoverKey = String.valueOf(map.get(playerUuidString));
+			if (player == null /* Player is offline, so a notification needs to be sent */ && ! playerPushoverKey.equals("INVALID"))
+			{
+				try
+				{
+					pushoverRestClient.pushMessage(PushoverMessage.builderWithApiToken("autoU13MYYXYxMaeupaqF7U7mBe9Bj")
+									.setUserId(playerPushoverKey)
+									.setSound("gamelan")
+									.setTitle("Minecraft" + titleEnd)
+									.setMessage(ChatColor.stripColor(message))
+									.build()
+					);
+				}
+				catch (PushoverException e)
+				{
+					if (e.getMessage().equals("user identifier is invalid"))
+					{
+						getUserKeysFileConfiguration().set(playerUuidString, "INVALID");
+						getAmountOfInvalidUsers();
+					}
+					else
+						e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void loadUserKeysFile() {
+		if (userKeysFile == null) {
+			userKeysFile = new File(getDataFolder(), "pushoveruserkeys.dat");
+		}
+		userKeysFileConfiguration = YamlConfiguration.loadConfiguration(userKeysFile);
+	}
+
+	public FileConfiguration getUserKeysFileConfiguration() {
+		if (userKeysFileConfiguration == null) {
+			loadUserKeysFile();
+		}
+		return userKeysFileConfiguration;
+	}
+
+	public void saveUserKeysFile() {
+		if (userKeysFileConfiguration == null || userKeysFile == null) {
+			return;
+		}
+		try {
+			getUserKeysFileConfiguration().save(userKeysFile);
+		} catch (IOException ex) {
+			getLogger().log(Level.SEVERE, "Could not save " + userKeysFile, ex);
+		}
+	}
+
+	public Player getOnlinePlayerByUuid(UUID uuid) {
+		for(Player p : getServer().getOnlinePlayers())
+			if (p.getUniqueId().equals(uuid))
+				return p;
+
+		return null;
+	}
+}
